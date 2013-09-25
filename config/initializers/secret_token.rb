1eb7fb1241267d621a010503af0d4d57aee359e5
Des Power <despower1@mac.com>
2013-09-25 09:51:10 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppThree::Application.config.secret_token = '950b1eae9f0cd3a430ce2e3588664a96c1d9f73c97dc00baff8b191b5b1127ffef701c46ef5599b62c76f2ed64a0ecefe7471ff3e40ecf0a173d6b7e103e1cb1'
